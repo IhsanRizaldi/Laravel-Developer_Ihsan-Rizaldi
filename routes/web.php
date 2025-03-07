7cf61054d36ec918e183446ae40ba2eb997ad885
IhsanRizaldi <rizaldiihsan1719@gmail.com>
2025-03-07 21:42:17 +0700

    Techical Test

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\PengeluaranController;
 use Illuminate\Support\Facades\Route;
 
 //Auth
-Route::get('/', [AuthController::class,'loginView'])->name('loginView')->middleware('guest');
-Route::post('/', [AuthController::class,'login'])->name('login')->middleware('guest');
-Route::get('/logout', [AuthController::class,'logout'])->name('logout')->middleware('auth');
+Route::get('/', [AuthController::class,'loginView'])->name('loginView');
+Route::post('/', [AuthController::class,'login'])->name('login');
+Route::get('/logout', [AuthController::class,'logout'])->name('logout');
 
 //Dashboard
 Route::get('/home', [DashboardController::class,'index'])->name('home')->middleware('auth');
@@ -23,7 +23,7 @@ Route::prefix('pendapatan')->group(function () {
     Route::get('/edit/{id}', [PendapatanController::class,'edit'])->name('pendapatan.edit');
     Route::post('/update/{id}', [PendapatanController::class,'update'])->name('pendapatan.update');
     Route::get('/destroy/{id}', [PendapatanController::class,'destroy'])->name('pendapatan.destroy');
-})->middleware('auth');
+});
 
 //Pengeluaran
 Route::prefix('pengeluaran')->group(function () {
@@ -33,4 +33,4 @@ Route::prefix('pengeluaran')->group(function () {
     Route::get('/edit/{id}', [PengeluaranController::class,'edit'])->name('pengeluaran.edit');
     Route::post('/update/{id}', [PengeluaranController::class,'update'])->name('pengeluaran.update');
     Route::get('/destroy/{id}', [PengeluaranController::class,'destroy'])->name('pengeluaran.destroy');
-})->middleware('auth');
+});
